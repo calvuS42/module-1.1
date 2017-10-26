e8f7f6e4e3045ddc32f6fdef3514f55ec66980e6
calvuS42 <olegka1302@gmail.com>
2017-10-26 21:25:58 +0300

    фінальний варіант, коефіціенти різні, щоб не зациклювався об'єкт

--- module-task-3/Source.cpp
+++ module-task-3/Source.cpp
@@ -10,16 +10,15 @@ void main()
 	cin >> x;
 	cout << "\t \t \t \t ";
 	cin >> y;
-	int mnX = -1, mnY = -1;
+	int mnX = -2, mnY = -1;
 	while(true){
 		printat(x, y, "*");
-		if (!x) mnX = 1;
-		if (!y) mnY = 1;
-		if (x > 30) mnX = -1;
-		if (y > 30) mnY = -1;
+		if (!x) mnX *= -1;
+		if (!y) mnY *= -1;
+		if (x > 30) mnX *= -1;
+		if (y > 30) mnY *= -1;
 		x += mnX;
 		y += mnY;
 		pause(100);
 	}
-	waitAnyKey();
 }
